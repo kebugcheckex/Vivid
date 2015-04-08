4cd6c31efb715831c1d7030a4dbc3883b66cb897
Xinyu Chen <collectchen@gmail.com>
2015-04-08 11:26:23 -0700

    Implemented GzVector class, but not tested.

--- Vivid/GzVector.cpp
+++ Vivid/GzVector.cpp
@@ -11,40 +11,145 @@
 
 GzVector::GzVector()
 	: GzPoint()
-{
+{	// Default constructor
 }
 
-GzVector::GzVector(float _x, float _y, float _z)
-	: GzPoint(_x, _y, _z)
-{
+GzVector::GzVector(float x, float y, float z, float w)
+	: GzPoint(x, y, z, w)
+{	// Initialize with x, y, z values
 }
 
-GzVector::GzVector(float coords, int dim)
+GzVector::GzVector(float *coords, int dim)
 	: GzPoint(coords, dim)
-{
+{	// Initialize with an array of float numbers
 }
 
 GzVector::GzVector(const GzPoint& begin, const GzPoint& end)
-{
-	x = end.x - begin.x;
-	y = end.y - begin.y;
-	z = end.z - begin.z;
-}
-
-GzVector::~GzVector()
-{
+{	// Initialize with two coordinates
+	X = end.X - begin.X;
+	Y = end.Y - begin.Y;
+	Z = end.Z - begin.Z;
+	W = end.W - begin.W;
 }
 
 GzVector GzVector::Normalize()
 {
-	float mag = sqrt(x*x + y*y + z*z);
-	x /= mag;
-	y /= mag;
-	z /= mag;
-	return *this;
+	float mag = GetMagnitude();
+	X /= mag;
+	Y /= mag;
+	Z /= mag;
+	return *this;	// This function both modifies the "this" class and return itself
 }
 
 float GzVector::GetMagnitude()
 {
-	return sqrt(x*x + y*y + z*z);
+	return sqrt(X*X + Y*Y + Z*Z);
+}
+
+float& GzVector::operator [] (const int index)
+{
+	switch (index)
+	{
+	case 0:
+		return X;
+		break;
+	case 1:
+		return Y;
+		break;
+	case 2:
+		return Z;
+		break;
+	case 3:
+		return W;
+		break;
+	default:
+		// TODO - Throw some exception, but I have not implemented exception class yet.
+		break;
+	}
+}
+
+void GzVector::operator - ()
+{
+	X = -X;
+	Y = -Y;
+	Z = -Z;
+	W = -W;
+}
+
+GzVector& GzVector::operator = (const GzVector& rhs)
+{
+	X = rhs.X;
+	Y = rhs.Y;
+	Z = rhs.Z;
+	W = rhs.W;
+	return *this;
+}
+
+inline GzVector& GzVector::operator += (const GzVector& rhs)
+{
+	X += rhs.X;
+	Y += rhs.Y;
+	Z += rhs.Z;
+	W += rhs.W;
+	return *this;
+}
+
+GzVector operator + (const GzVector& lhs, const GzVector& rhs)
+{
+	GzVector ret{ lhs };
+	return ret += rhs;
+}
+
+GzVector& GzVector::operator -= (const GzVector& rhs)
+{
+	X -= rhs.X;
+	Y -= rhs.Y;
+	Z -= rhs.Z;
+	W -= rhs.W;
+	return *this;
+}
+
+GzVector operator - (const GzVector& lhs, const GzVector& rhs)
+{
+	GzVector ret{ lhs };
+	return ret -= rhs;
+}
+
+GzVector& GzVector::operator *= (const float& rhs)
+{
+	X *= rhs;
+	Y *= rhs;
+	Z *= rhs;
+	W *= rhs;
+	return *this;
+}
+
+GzVector operator * (const GzVector& lhs, const float& rhs)
+{
+	GzVector ret{ lhs };
+	return ret *= rhs;
+}
+
+GzVector operator * (const float& lhs, const GzVector& rhs)
+{
+	GzVector ret{ rhs };
+	return ret *= lhs;
+}
+
+float operator * (const GzVector& lhs, const GzVector& rhs)
+{
+	float ret = lhs.X * rhs.X
+		+ lhs.Y * rhs.Y
+		+ lhs.Z * rhs.Z
+		+ lhs.W * rhs.W;
+	return ret;
+}
+
+GzVector operator ^ (const GzVector& lhs, const GzVector& rhs)
+{	// For cross-product, we only do three dimensional, ignoring w value
+	float x = lhs.Y*rhs.Z - lhs.Z*rhs.Y;
+	float y = lhs.Z*rhs.X - lhs.X*rhs.Z;
+	float z = lhs.X*rhs.Y - lhs.Y*rhs.X;
+	GzVector ret{ x, y, z };
+	return ret;
 }
--- Vivid/GzVector.h
+++ Vivid/GzVector.h
@@ -13,35 +13,60 @@ class GzVector :
 	public GzPoint
 {
 public:
-	// Constructors
+	// Default constructor
 	GzVector();
-	/* Initialize with x, y, z values. */
-	GzVector(float _x, float _y, float _z);
-	/* Initialize with an array of float numbers. */
+
+	// Initialize with x, y, z values
+	GzVector(float x, float y, float z = 0, float w = 0);
+
+	// Initialize with an array of float numbers
 	GzVector(float *coords, int dim);
-	/* Initialize with two coordinates. */
+
+	// Initialize with two coordinates
 	GzVector(const GzPoint& begin, const GzPoint& end);
-	// Destructor
-	~GzVector();
-public:
-	// Operators
-	float& operator [] (int index);
-	GzVector operator - ();	// Unary minus
-	GzVector operator = (const GzVector& rhs);	// Value copy
-	friend GzVector operator + (const GzVector& lhs, const GzVector& rhs);	// Vector addition
-	GzVector operator += (const GzVector& rhs);		// Add to itself
-	friend GzVector operator - (const GzVector& lhs, const GzVector& rhs);	// Vector subtraction
-	GzVector operator -= (const GzVector& rhs);		// Subtract from itself
-	friend GzVector operator * (const GzVector& lhs, const float& rhs);		// Scalar product (scalar on the right)
-	GzVector operator *= (const float& rhs);		// Scalar product to itself (scalar on the right)
-	friend GzVector operator * (const float& lhs, const GzVector& rhs);		// Scalar product (scalar on the left)
-	friend float operator * (const GzVector& lhs, const GzVector& rhs);		// Dot product
-	friend GzVector operator ^ (const GzVector& lhs, const GzVector& rhs);	// Cross product
+
+	// Subscript operator, supports both read and write
+	float& operator [] (const int index);
+
+	// Unary minus
+	void operator - ();
+
+	// Value copy
+	GzVector& operator = (const GzVector& rhs);
+
+	// Add to itself
+	GzVector& operator += (const GzVector& rhs);
+
+	// Vector addition
+	friend GzVector operator + (const GzVector& lhs, const GzVector& rhs);
+	
+	// Subtract from itself
+	GzVector& operator -= (const GzVector& rhs);
+
+	// Vector subtraction
+	friend GzVector operator - (const GzVector& lhs, const GzVector& rhs);
+	
+	// Scalar product to itself (scalar on the right)
+	GzVector& operator *= (const float& rhs);
+
+	// Scalar product (scalar on the right)
+	friend GzVector operator * (const GzVector& lhs, const float& rhs);
+	
+	// Scalar product (scalar on the left)
+	friend GzVector operator * (const float& lhs, const GzVector& rhs);
+
+	// Dot product
+	friend float operator * (const GzVector& lhs, const GzVector& rhs);
+
+	// Cross product
+	friend GzVector operator ^ (const GzVector& lhs, const GzVector& rhs);
+
 	//GzVector operator ^= (const GzVector& rhs);
 	/*
 		Cross-product is not commutative. Therefore it is not a good idea to implement
 		a ^= operator.
 	*/
+
 	//GzVector operator / (const float& rhs);
 	/*
 		In order to prevent confusion, I decide not to define a division operation
@@ -50,6 +75,7 @@ public:
 
 	/* Normalize the vector, return itself. */
 	GzVector Normalize();
+
 	/* Get the magnitude of the vector. */
 	float GetMagnitude();
 };
